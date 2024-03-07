401e41b459f60ebce5f1491493af6b702c00e0da
rahuls75 <161546561+rahuls75@users.noreply.github.com>
2024-03-07 19:00:59 +0530

    master commit

---
+++ src/insuranceproject/Healthinsurance.java
@@ -0,0 +1,12 @@
+package insuranceproject;
+
+public class Healthinsurance {
+
+	public static void main(String[] args) {
+
+		System.out.println("user is at insurance page");
+		System.out.println(" master branch level");
+		
+	}						
+
+}
