0392153d32f1a13efc747f0bcfbaf9fa1e2cb0a4
rahuls75 <161546561+rahuls75@users.noreply.github.com>
2024-03-07 19:11:04 +0530

    changes done at master branch

--- src/insuranceproject/Healthinsurance.java
+++ src/insuranceproject/Healthinsurance.java
@@ -5,7 +5,7 @@ public class Healthinsurance {
 	public static void main(String[] args) {
 
 		System.out.println("user is at insurance page");
-		System.out.println(" master branch level");
+		System.out.println(" some changes at master branch level");
 		
 	}						
 
