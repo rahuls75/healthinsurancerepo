b8d247cadfef0a927b44ac0273deb66a8fae29a8
rahuls75 <161546561+rahuls75@users.noreply.github.com>
2024-03-07 19:06:06 +0530

    insurancebranch commit

--- src/insuranceproject/Healthinsurance.java
+++ src/insuranceproject/Healthinsurance.java
@@ -5,7 +5,7 @@ public class Healthinsurance {
 	public static void main(String[] args) {
 
 		System.out.println("user is at insurance page");
-		System.out.println(" master branch level");
+		System.out.println(" personal branch level");
 		
 	}						
 
